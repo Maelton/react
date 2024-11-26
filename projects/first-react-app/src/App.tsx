663683288ae526e659cfe527fe880a6c61d2a38a
Maelton <maelton.main@gmail.com>
2024-11-26 18:04:58 -0300

    :sparkles: (first-react-app/): implemented event handlers

--- projects/first-react-app/src/App.tsx
+++ projects/first-react-app/src/App.tsx
@@ -1,3 +1,36 @@
+import { MouseEvent } from "react";
+
 export default function App() {
-    return <h1>Hellow world!</h1>;    
+    return (
+        <Button />
+    );
 }
+
+const Button = () => {
+    let counter: number = 0;
+
+    const iRecieveAMouseEvent = (e: MouseEvent<HTMLButtonElement>) => console.log(e.target);
+
+    return (
+        <>  
+            <button onClick={
+                (eventObject: MouseEvent<HTMLButtonElement>) => {
+                counter++;
+                alert(counter);
+                console.log(`Im button add and my event is: ${eventObject.target}`);
+            }}>
+                Add
+            </button>
+            <button onClick={
+                (e) => {
+                counter--;
+                alert(counter);
+
+                //React automatically identifies I expect a MouseEvent and provides it to me
+                iRecieveAMouseEvent(e);
+            }}>
+                Subtract
+            </button>
+        </>
+    );
+};
