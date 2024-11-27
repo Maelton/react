163c0e862a268c41fa87a184fc5de8bafd7fef48
Maelton <maelton.main@gmail.com>
2024-11-27 08:21:35 -0300

    :sparkles: (first-react-app/): useState()

--- projects/first-react-app/src/App.tsx
+++ projects/first-react-app/src/App.tsx
@@ -1,36 +1,22 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 export default function App() {
+
+    let [counter, setCounter] = useState<number>(0);
+
     return (
-        <Button />
+        <div>
+            <h1>Counter: {counter}</h1>
+            
+            <br/>
+            
+            <button onClick={ () => setCounter(counter + 1) }>
+                ADD
+            </button>
+
+            <button onClick={ () => setCounter(counter - 1) }>
+                SUBTRACT
+            </button>
+        </div>
     );
 }
-
-const Button = () => {
-    let counter: number = 0;
-
-    const iRecieveAMouseEvent = (e: MouseEvent<HTMLButtonElement>) => console.log(e.target);
-
-    return (
-        <>  
-            <button onClick={
-                (eventObject: MouseEvent<HTMLButtonElement>) => {
-                counter++;
-                alert(counter);
-                console.log(`Im button add and my event is: ${eventObject.target}`);
-            }}>
-                Add
-            </button>
-            <button onClick={
-                (e) => {
-                counter--;
-                alert(counter);
-
-                //React automatically identifies I expect a MouseEvent and provides it to me
-                iRecieveAMouseEvent(e);
-            }}>
-                Subtract
-            </button>
-        </>
-    );
-};
